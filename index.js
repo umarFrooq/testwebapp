9e681f57d80e7e7957a9a2143d4a0cdd4a098d7b
umarFrooq <115084296+umarFrooq@users.noreply.github.com>
2025-08-12 00:02:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('hi i ma runging');
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+{
+  "name": "simple-backend",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"No tests specified\""
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
